1d37c851feb11b361f4e9c3d040f3991c2edd502
Thuthur22 <102926386+Thuthur22@users.noreply.github.com>
2023-03-18 11:55:05 +0100

    Add files via upload

--- analyseur_lexical.h
+++ analyseur_lexical.h
@@ -1,1 +1,13 @@
-int fonction1();
+#ifndef analyseur_lexical_h
+#define analyseur_lexical_h
+
+#include "jeton.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void analyse_lexicale(typejeton tab[], int *erreur_pg, char c[]);
+
+Arbre creer_noeud(typejeton T,Arbre fg,Arbre fd);
+
+#endif
